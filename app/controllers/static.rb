5a4070293100b9a35196ad2c08fb06c1bbce9ae4
Your Name <you@example.com>
2017-05-16 17:28:57 +0800

    added coding with basic css

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,3 +1,46 @@
+enable :sessions
 get '/' do
+	@url = Url.all.limit(7)
   erb :"static/index"
+end
+
+get '/profile' do
+ erb :"static/profile"
+end
+
+post "/submit" do 
+	long = Url.new(long_url: params[:long_url])
+	
+	if long.save
+		# @error = false
+		flash[:success] = "Congrats, you created a bitly link."
+	else
+		flash[:error] = "Woops, something went wrong."
+		flash[:danger] = "Please type correct link"
+		# @error = true
+	end
+	redirect to '/'
+end
+
+# post '/long_url' do
+# 	x = Url.new
+# 	x.long_url = params[:long_url]
+# 	x.shorten
+
+# 	if x.save
+# 		@error = false
+# 	else
+# 		@error = true
+# 	end
+# 	@urls = [x]
+# 	erb :"static/index"
+# end
+
+
+get '/:short_url' do
+
+	link = Url.find_by(short_url: params[:short_url])
+	link.count_click += 1
+	link.save
+	redirect to "http://" + link.long_url
 end
