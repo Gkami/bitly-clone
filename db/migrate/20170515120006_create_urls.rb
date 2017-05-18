706c9e463c9470f78d5c4b1c415091741f431547
Your Name <you@example.com>
2017-05-18 10:21:30 +0800

    add version

--- db/migrate/20170515120006_create_urls.rb
+++ db/migrate/20170515120006_create_urls.rb
@@ -1,4 +1,4 @@
-class CreateUrls < ActiveRecord::Migration
+class CreateUrls < ActiveRecord::Migration[5.0]
 	def change
 		create_table :urls do |x|
 			x.string  :long_url
--- db/migrate/20170516091914_add_count_to_urls.rb
+++ db/migrate/20170516091914_add_count_to_urls.rb
@@ -1,4 +1,4 @@
-class AddCountToUrls < ActiveRecord::Migration
+class AddCountToUrls < ActiveRecord::Migration[5.0]
 	def change
 		add_column :urls, :count_click, :integer, default: 0 # table name,column name & data type of column 
 	end
